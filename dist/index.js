0f92ccc1a7c0697de63fbf7683708ddc8e455db0
remal <remal@users.noreply.github.com>
2022-09-19 23:37:15 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -924,7 +924,7 @@ async function run() {
                 }
             }
         }
-        let releaseDescription = `${releaseTag}`;
+        let releaseDescription = 'No release notes';
         if (changeLogItems.length) {
             releaseDescription = '# What\'s Changed\n';
             for (const changeLogItem of changeLogItems) {
