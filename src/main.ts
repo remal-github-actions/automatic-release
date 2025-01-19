e8b8b6b5739ecffd9dc0052d089083c77861faff
Semyon Levin <levin.semen@gmail.com>
2025-01-19 15:28:17 -0800

    loging

--- src/main.ts
+++ src/main.ts
@@ -201,7 +201,7 @@ async function run(): Promise<void> {
             author: string | null | undefined = undefined,
             pullRequestNumber: number | null | undefined = undefined,
         ) {
-            core.info(`Registering changelog item: ` + [
+            core.info(`  Registering changelog item: ` + [
                 `commit='${commit.sha}'`,
                 `type='${type}'`,
                 `message='${message}'`,
@@ -222,7 +222,7 @@ async function run(): Promise<void> {
                         || messageAfterPrefix.match(/^\W/)
                         || skippedChangelogCommitPrefix.match(/\W$/)
                     ) {
-                        core.info(`Excluding changelog message by prefix '${skippedChangelogCommitPrefix}': ${originalMessage}`)
+                        core.info(`  Excluding changelog message by prefix '${skippedChangelogCommitPrefix}': ${originalMessage}`)
                         return
                     }
                 }
